46d9fa876df40b544e4fb47e9bf70555f59a90f3
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-10-25 16:34:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Pages/HomePage/HomePage';
+import ProductViewPage from './Pages/ProductViewPage/ProductViewPage';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSlidersH,faHouseUser,faUserLock,faRupeeSign ,faMinus,faPlus,faTrashAlt,faBars,faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import SignInPage from './Pages/SignInPage/SignInPage';
+
+library.add(faSlidersH, faHouseUser, faUserLock,faRupeeSign,faMinus,faPlus,faTrashAlt,faBars,faShoppingCart)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+      <Switch>
+        <Route path='/' exact>
+          <Homepage/>
+        </Route>
+        <Route path='/CoderView' >
+          <ProductViewPage/>
+        </Route>
+        <Route path='/SignInPage'>
+          <SignInPage/>
+        </Route>
+        <Route path='*'>
+          <PageNotFound/>
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
